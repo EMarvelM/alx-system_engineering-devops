aeca213a7047b8028df15354a9e4b6235938ebc7
emarvelm <marvelousegbe19@gmail.com>
2024-03-27 12:49:13 +0100

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,11 +1,20 @@
-mode default{
-package { 'nginx':
-ensure => 'installed',
+#setup nginx
+
+package {
+    'nginx':
+    ensure => installed,
 }
 
-file {'/var/www/html':
-content => 'Hello World!'
-mode => '0644',
+file {'/var/www/html/index.nginx-debian.html':
+    content => 'Hello World!',
 }
 
+file_line {'configure redirection':
+    path  =>  '/etc/nginx/sites-available/default',
+    after =>  'server_name _;',
+    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtu.be/dQw4w9WgXcQ;\n\t}\n",
+}
+
+service {'nginx':
+    ensure => running,
 }
