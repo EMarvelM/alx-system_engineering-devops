0d3eafb82f3b7927f65e0863aedeba2b2ed7425c
emarvelm <marvelousegbe19@gmail.com>
2024-04-23 12:57:48 +0100

    pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""using this REST API, for a given employee ID, returns information about his/her TODO list progress.
+"""using this REST API, for a given employee ID, returns
+   information about his/her TODO list progress.
 """
 import requests
 from sys import argv
@@ -10,8 +11,9 @@ if __name__ == "__main__":
     r = requests.get(u_link)
     r_user = r.json()['name']
 
-    t_link = requests.get("https://jsonplaceholder.typicode.com/user/{}/todos".format(num))
-    t_link_data = t_link.json()
+    t_link = "https://jsonplaceholder.typicode.com/user/{}/todos".format(num)
+    t_l = requests.get(t_link)
+    t_link_data = t_l.json()
     t_lengh = len(t_link_data)
     lengh = 0
     task = []
@@ -21,6 +23,7 @@ if __name__ == "__main__":
             task.append(i['title'])
         pass
 
-    print("Employee {} is done with tasks({}/{}):".format(r_user, lengh, t_lengh))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(r_user, lengh, t_lengh))
     for i in task:
         print("\t {}".format(i))
