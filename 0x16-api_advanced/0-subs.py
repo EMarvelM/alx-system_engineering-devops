e56522cfa76111d4c62306fd07208427819f3bfc
EMarvelM <marvelousegbe19@gmail.com>
2024-05-08 05:51:21 +0100

    queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-""" queries the Reddit API and returns the number of subscribers """
+"""function that queries the Reddit API and returns the number of subscribers
+for a given subreddit"""
 
-from sys import argv
 import requests
 
+
 def number_of_subscribers(subreddit):
-    r_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    header = {'user-agent': 'Mozilla/5.0'}
-
-    r = requests.get(r_url, headers=header)
-
-    if r.status_code == 200:
-        return len(r.json()['data'])
-    else
+    """function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
+        return 0
